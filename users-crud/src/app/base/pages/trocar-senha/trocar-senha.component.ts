4d3cc9c2879f6821f4b5473acbec8774f452de09
Vinicus Martins Correia <vinix.correia@gmail.com>
2022-08-31 22:21:51 -0300

    change message

--- users-crud/src/app/base/pages/trocar-senha/trocar-senha.component.ts
+++ users-crud/src/app/base/pages/trocar-senha/trocar-senha.component.ts
@@ -7,29 +7,49 @@ import { GenericService } from '../../services/generic.service';
 @Component({
   selector: 'app-trocar-senha',
   templateUrl: './trocar-senha.component.html',
-  styleUrls: ['./trocar-senha.component.css']
+  styleUrls: ['./trocar-senha.component.css'],
 })
 export class TrocarSenhaComponent implements OnInit {
   formSenha: FormGroup;
 
-  constructor(public swalService: SwalService, public service: GenericService, public dialogRef: MatDialogRef<TrocarSenhaComponent>,) { }
+  constructor(
+    public swalService: SwalService,
+    public service: GenericService,
+    public dialogRef: MatDialogRef<TrocarSenhaComponent>
+  ) {}
 
   ngOnInit(): void {
     this.formSenha = new FormGroup({
-      novaSenha: new FormControl('', [Validators.required, Validators.minLength(8), Validators.pattern(RegExp(/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[-+_!@#$%^&.,?])(?!.*[\s]).+$/))]),
-    })
+      novaSenha: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.pattern(
+          RegExp(
+            /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[-+_!@#$%^&.,?])(?!.*[\s]).+$/
+          )
+        ),
+      ]),
+    });
   }
 
   save() {
     if (this.formSenha.invalid) {
-      this.swalService.error("Erro", "Preencha o campo senha corretamente.", "Ok");
+      this.swalService.error(
+        'Erro',
+        'Formato de senha inválido! A senha deve ter no mínimo 8 caracteres, atendendo aos critérios: ao menos 1 letra maiúscula, ao menos 1 letra minúscula, no mínimo 1 digito numérico. Caracteres em branco não são aceitos!',
+        'Ok'
+      );
     }
     this.service.trocaSenha(this.formSenha.value.novaSenha).subscribe({
       next: (data) => {
-        this.swalService.success("Concluído", "Senha atualizada com sucesso.", "Ok");
+        this.swalService.success(
+          'Concluído',
+          'Senha atualizada com sucesso.',
+          'Ok'
+        );
         this.dialogRef.close();
-      }
-    })
+      },
+    });
   }
 
   cancel() {
